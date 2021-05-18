a2f5809a038f44d0d4edbbacd4db36f58075d8a4
trevorjpt33 <33333712+trevorjpt33@users.noreply.github.com>
2021-05-18 14:06:01 -0500

    Add files

---
+++ main.py
@@ -0,0 +1,121 @@
+# Trevor Poirier
+# LP Technologies Coding Challenge
+# Tuesday May 18th, 2021
+# Programming language chosen: Python
+# Total time spent: 5 hours
+
+import matplotlib
+import matplotlib.pyplot as plt
+import mysql.connector
+import numpy as np
+import time
+from array import array
+from os import getcwd
+from pathlib import Path as plib
+
+matplotlib.use('TkAgg') # Integrate matplotlib with Python's Tkinter GUI library
+current_dir = plib(getcwd())
+bytes_file = plib.joinpath(current_dir, 'MySQL_Data')
+print(bytes_file, current_dir)
+
+try:
+    # Try establishing a connection to the database
+    lp = mysql.connector.connect(
+        host="localhost",
+        auth_plugin='mysql_native_password',
+        user="Trevor",
+        passwd="LPTCodeChallenge",
+        database="lp")
+    cursor = lp.cursor()
+except mysql.connector.Error as DbConnectError:
+    # If unsuccessful, show user the error message
+    print("\nUnable to connect to database successfully.", "\n---> " + DbConnectError.msg)
+
+
+# Queries the database in MySQL and writes the result to a binary file #
+def read_db_data(outfile):
+    blobs_count = 0  # int: number of rows
+    blobs_size = 0   # int: length in bytes of each BLOB
+
+    with open(outfile, 'wb') as f:
+        try:
+            query = "SELECT trace_data FROM test ORDER BY trace_id ASC;"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            blobs_count = len(result)
+
+            for blob in result:
+                blobs_size = len(blob[0])
+                f.write(blob[0])
+
+        except mysql.connector.Error as DbQueryError:
+            print("\nThe provided query was not executed successfully.", "\n---> " + DbQueryError.msg)
+
+    return blobs_count, blobs_size
+
+
+# Reads binary file directly into an array of long integers for each row of the resulting relation #
+def parse_db_data(infile, blobs_count, blobs_size):
+    draws = []  # list that will contain arrays for each BLOB
+    with open(infile, 'rb') as f:
+        for i in range(blobs_count):
+            arr = array('l', f.read(blobs_size))  # Read bytes into an array - byte order is little endian by default
+            arr.byteswap()                        # Change order to big endian, which is byte order of original data
+            draws.append(arr)
+
+    return draws
+
+
+# Both of these lists will be utilized when drawing the graph - they determine which tick marks have values
+ticks_x = ['850 MHZ', ' ', ' ', ' ', ' ', '1000 MHZ', ' ', ' ', ' ', ' ', '1150 MZ']  # ' ' = tick marks with no value
+ticks_y = ['-130 dBm', '-120 dBm', '-110 dBm', '-100 dBm', '-90 dBm', '-80 dBm',
+           '-70 dBm','-60 dBm', '-50 dBm', '-40 dBm', '-30 dBm']
+
+fig = plt.figure("LP Technologies")      # Assign the matplotlib figure to a variable
+ax = fig.add_subplot()  # Initialize a subplot (axes) for more customization later
+
+num_blobs, size_blobs = read_db_data(bytes_file)
+data = parse_db_data(bytes_file, num_blobs, size_blobs)
+
+doubles = []
+# Iterate over the 50 individual arrays within the outer list
+for blob in data:
+    points = []
+    # Iterate over the long integers within each of the 50 individual arrays
+    for point in blob:
+        points.append(point / 1000)
+    doubles.append(points)
+
+while True:    # Begin while loop to ensure the drawings are repeated until user exits the application
+    plt.ion()  # Enable interactive mode where figures are automatically shown
+    for i in range(len(doubles)):
+        fig.patch.set_facecolor('black')  # Set window background color to black
+        ax.patch.set_facecolor('black')   # Set graph background color to black
+        x = np.arange(850, 1150.5, 0.5)   # Generate an array of values for x axis with same dimensions as y axis
+        y = doubles[i]
+
+        # Change each "spine" or outer edge of the graph to gray dashed line
+        ax.spines['top'].set_linestyle('dashed')
+        ax.spines['top'].set_edgecolor('gray')
+        ax.spines['left'].set_linestyle('dashed')
+        ax.spines['left'].set_edgecolor('gray')
+        ax.spines['right'].set_linestyle('dashed')
+        ax.spines['right'].set_edgecolor('gray')
+        ax.spines['bottom'].set_linestyle('dashed')
+        ax.spines['bottom'].set_edgecolor('gray')
+
+        # Set lower and upper limits of each of the axes
+        plt.xlim(850, 1150)
+        plt.ylim(-130, -30)
+
+        # For both, first argument is all of the tick marks to be shown
+        # Second argument adds a number (and its units) to specific tick marks 
+        plt.xticks(range(850, 1180, 30), ticks_x, color='white') 
+        plt.yticks(range(-130, -20, 10), ticks_y, color='white')
+        
+        plt.grid(color='gray', linestyle='--')          # Show gray dashed gridlines according to xticks and yticks
+        plt.plot(x, y, color='yellow', linewidth=0.75)  # Show the trace for the current index
+        plt.pause(1.0)                                  # Display the current trace for 1 second
+        plt.cla()                                       # Clear the current trace to display the next one
+
+    time.sleep(2.0)  # Hold for a couple seconds after last trace before starting over
